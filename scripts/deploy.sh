1b457be4a0a45d02aa8423053027526b1b7d398b
Vladislav Fisun <vladislav.fisun@rt.ru>
2024-09-29 00:30:19 +0300

    add physics mode

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,10 @@ COMPOSE_FILE="docker-compose.yaml"
 
 cd "$(dirname "$0")/../" || exit
 
+git fetch
+
+git pull origin master
+
 echo "Starting deployment..."
 
 docker-compose -f "$COMPOSE_FILE" down
