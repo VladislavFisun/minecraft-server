d8554c7d0f705c8f72c8d43b51ceecd95616485b
Vladislav Fisun <vladislav.fisun@rt.ru>
2024-09-28 21:59:57 +0300

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,13 +7,13 @@ git pull
 git log
 echo "Starting deployment..."
 
-sudo docker-compose -f "$COMPOSE_FILE" down
+docker-compose -f "$COMPOSE_FILE" down
 
-sudo docker-compose -f "$COMPOSE_FILE" up -d --build
+docker-compose -f "$COMPOSE_FILE" up -d --build
 
-sudo docker-compose -f "$COMPOSE_FILE" logs -f
+docker-compose -f "$COMPOSE_FILE" logs -f
 sleep 10
-sudo docker-compose -f "$COMPOSE_FILE" ps
-sudo docker-compose -f "$COMPOSE_FILE" logs --tail=100
+docker-compose -f "$COMPOSE_FILE" ps
+docker-compose -f "$COMPOSE_FILE" logs --tail=100
 
 echo "Deployment completed!"eted!"
