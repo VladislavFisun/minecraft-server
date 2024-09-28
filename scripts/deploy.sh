772a1c878e2a3f8c21a203437d0db3fa661cfb42
Vladislav Fisun <vladislav.fisun@rt.ru>
2024-09-28 21:48:15 +0300

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,12 +4,6 @@ COMPOSE_FILE="docker-compose.yaml"
 
 cd "$(dirname "$0")/../" || exit
 
-git fetch
-
-git pull origin master || { echo "Git pull failed"; exit 1; }
-
-git log
-
 echo "Starting deployment..."
 
 sudo docker-compose -f "$COMPOSE_FILE" down
