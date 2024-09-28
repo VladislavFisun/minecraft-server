3742a693856ffd471f690116422965f3fe3f4a5e
Vladislav Fisun <vladislav.fisun@rt.ru>
2024-09-28 20:20:35 +0300

    echo test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,12 +4,16 @@ COMPOSE_FILE="docker-compose.yaml"
 
 cd "$(dirname "$0")/../" || exit
 
+git fetch
+
+git pull
+
 echo "Starting deployment..."
 
-docker-compose -f "$COMPOSE_FILE" down
+sudo docker-compose -f "$COMPOSE_FILE" down
 
-docker-compose -f "$COMPOSE_FILE" up -d --build
+sudo docker-compose -f "$COMPOSE_FILE" up -d --build
 
-docker-compose -f "$COMPOSE_FILE" logs -f
+sudo docker-compose -f "$COMPOSE_FILE" logs -f
 
 echo "Deployment completed!"
